305c45bb30596863cd0cb8f388045b7e0ef9ac7c
luwasu <luis.santanaholmes@gmail.com>
2020-07-06 17:59:18 +0200

    add first int variable

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
 
     public static void main(String[] args) {
+        // print out Hello World to the console
         System.out.println("Hello Luis!");
+
+        int myFirstNumber = 10 + 5;
+
+        System.out.println("My first number is " + myFirstNumber);
     }
 }
