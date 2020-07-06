eeff3fa8b24422d768a3f2212aa155fdcfec264f
luwasu <luis.santanaholmes@gmail.com>
2020-07-06 21:49:12 +0200

    add updated exercise

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,8 +4,16 @@ public class Hello {
         // print out Hello World to the console
         System.out.println("Hello Luis!");
 
-        int myFirstNumber = 10 + 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastValue = 1000 - myTotal;
 
         System.out.println("My first number is " + myFirstNumber);
+        System.out.println("My total number is " + myTotal);
+        System.out.println("My last value is " + myLastValue);
+
+
     }
 }
